9436d8af3d7bee97a285e1d7c6dc087fd85f44a9
Matias <dmaffoni@hotmail.com>
2022-08-16 00:12:08 -0300

    Cambios 4

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgentinaComponent } from './components/logo-argentina/logo-argentina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgentinaComponent
   ],
   imports: [
     BrowserModule,
