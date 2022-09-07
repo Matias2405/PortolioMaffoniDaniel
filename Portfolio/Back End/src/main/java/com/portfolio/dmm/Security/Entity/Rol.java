53e417f60b5d116fac536cf0499fd90a902c4ef1
Matias <dmaffoni@hotmail.com>
2022-09-07 19:37:32 -0300

    Cambios 16

---
+++ Portfolio/Back End/src/main/java/com/portfolio/dmm/Security/Entity/Rol.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.dmm.Security.Entity;
+
+import com.portfolio.dmm.Security.Enums.Rolnombre;
+import javax.persistence.Entity;
+
+@Entity
+public class Rol {
+    private int id;
+    private Rolnombre rolnombre;
+}
